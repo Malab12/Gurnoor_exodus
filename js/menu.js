3d08a3b392d309111eadd3780101bf894c200cf4
Agastya <agastya2002@gmail.com>
2020-10-04 14:57:03 +0530

    Menu done

--- js/menu.js
+++ js/menu.js
@@ -182,11 +182,13 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  location.reload();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    location.reload();
   }
 }
